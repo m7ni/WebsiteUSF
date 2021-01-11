443852d9cc51c4bdad4ac76100b3b0d1077ab36e
m7ni <a2020142829@isec.pt>
2021-01-11 15:29:33 +0000

    indicadores feito

--- Indicadores/Indicadoresjava.js
+++ Indicadores/Indicadoresjava.js
@@ -1,6 +1,11 @@
 
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(sexo);
+google.charts.setOnLoadCallback(idade);
+google.charts.setOnLoadCallback(consultas);
+google.charts.setOnLoadCallback(especialidade);
+google.charts.setOnLoadCallback(visitas);
+google.charts.setOnLoadCallback(custo);
 
 function sexo() {
 
@@ -19,8 +24,6 @@ function sexo() {
 
   chart.draw(data, options);
 }
-
-
 
   function idade() {
 
@@ -44,3 +47,116 @@ function sexo() {
     chart.draw(data, options);
   }
   
+
+  function consultas() {
+    var data = google.visualization.arrayToDataTable([
+      ["ano", "Consultas por utente", { role: "style" } ],
+      ["1993", 2.5, "#2C2C2C"],
+      ["2000", 2.8, "#878787"],
+      ["2010", 3.1, "#2C2C2C"],
+      ["2020", 3.7, "#878787"]
+    ]);
+
+    var view = new google.visualization.DataView(data);
+    view.setColumns([0, 1,
+                     { calc: "stringify",
+                       sourceColumn: 1,
+                       type: "string",
+                       role: "annotation" },
+                     2]);
+
+    var options = {
+      title: "Consultas por Utente",
+     
+      bar: {groupWidth: "95%"},
+      legend: { position: "none" },
+    };
+    var chart = new google.visualization.ColumnChart(document.getElementById("consultas_graph"));
+    chart.draw(view, options);
+  }
+
+  function especialidade() {
+    var data = google.visualization.arrayToDataTable([
+      ['Ano', 'Medicina geral e familiar', 'Estomatologia e medicina dentária', 'Planeamento familiar', 'Pediatria', 'Saúde materna'],
+      ['1993',  2878,      2102,         555,             4316,           734],
+      ['2020',  4074,      428,         1196,             3128,          662]
+    ]);
+
+    var options = {
+      title : 'Consultas Medicas Por Especialidade',
+      vAxis: {title: 'Total'},
+      hAxis: {title: 'Ano'},
+      seriesType: 'bars',
+      series: {5: {type: 'line'}}
+    };
+
+    var chart = new google.visualization.ComboChart(document.getElementById('especialidade_graph'));
+    chart.draw(data, options);
+  }
+
+  function visitas() {
+    var data = google.visualization.arrayToDataTable([
+      ["ano", "Taxa de visitas domiciliárias médicas", { role: "style" } ],
+      ["1993", 52, "#2C2C2C"],
+      ["2000", 60, "#878787"],
+      ["2010", 92, "#2C2C2C"],
+      ["2020", 112, "#878787"]
+    ]);
+
+    var view = new google.visualization.DataView(data);
+    view.setColumns([0, 1,
+                     { calc: "stringify",
+                       sourceColumn: 1,
+                       type: "string",
+                       role: "annotation" },
+                     2]);
+
+    var options = {
+      title: "Taxa de visitas domiciliárias médicas",
+     
+      bar: {groupWidth: "95%"},
+      legend: { position: "none" },
+    };
+    var chart = new google.visualization.ColumnChart(document.getElementById("visitas_graph"));
+    chart.draw(view, options);
+  }
+
+
+
+  function custo() {
+    var data = google.visualization.arrayToDataTable([
+      ["ano", "Custo médio total por consulta médica", { role: "style" } ],
+      ["1993", 60 , "#2C2C2C"],
+      ["2000",  100, "#878787"],
+      ["2010",  256, "#2C2C2C"],
+      ["2020",  323, "#878787"]
+    ]);
+
+    var view = new google.visualization.DataView(data);
+    view.setColumns([0, 1,
+                     { calc: "stringify",
+                       sourceColumn: 1,
+                       type: "string",
+                       role: "annotation" },
+                     2]);
+
+    var options = {
+      title: "Custo médio total por consulta médica",
+     
+      bar: {groupWidth: "95%"},
+      legend: { position: "none" },
+    };
+    var chart = new google.visualization.ColumnChart(document.getElementById("custo_graph"));
+    chart.draw(view, options);
+  }
+
+
+
+$(window).resize(function(){
+  sexo();
+  idade();
+  consultas();
+  especialidade();
+  visitas();
+  custo();
+});
