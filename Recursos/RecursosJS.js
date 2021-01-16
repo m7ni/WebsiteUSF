6afaed86556af7b31e263239019513211781331c
Doli00 <75916279+Doli00@users.noreply.github.com>
2021-01-16 20:24:19 +0000

    ads

--- Recursos/RecursosJS.js
+++ Recursos/RecursosJS.js
@@ -1,29 +1,36 @@
-window.onload = function () {
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawBackgroundColor);
 
-    var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        title:{
-            text: "Previsão do número de consultas diárias médias para os próximos 5 anos"
+function drawBackgroundColor() {
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'X');
+      data.addColumn('number', 'Dogs');
+
+      data.addRows([
+        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+        [66, 70], [67, 72], [68, 75], [69, 80]
+      ]);
+
+      var options = {
+        hAxis: {
+          title: 'Time'
         },
-        data: [{        
-            type: "line",
-              indexLabelFontSize: 16,
-            dataPoints: [
-                { x:2015 , y: 15 },
-                { x:2016 , y: 13 , indexLabel: "\u2193 lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
-                { x:2017 , y: 16},
-                { x:2018 , y: 15 },
-                { x:2019 , y: 14 },
-                { x:2020 , y: 18 },
-                { x:2021 , y: 20 },
-                { x:2022 , y: 21 },
-                { x:2023 , y: 19 },
-                { x:2024 , y: 23 },
-                { x:2025 , y: 25 },
-                { x:2026 , y: 26 , indexLabel: "\u2191 highest",markerColor: "red", markerType: "triangle" },
-            ]
-        }]
-    });
-    chart.render();
-    
+        vAxis: {
+          title: 'Popularity'
+        },
+        backgroundColor: '#f1f8e9'
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
     }
