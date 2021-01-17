b99dd75fef5595bf25c99013a68f549dd7b1f434
m7ni <a2020142829@isec.pt>
2021-01-17 00:08:41 +0000

    jj

--- Recursos/RecursosJS.js
+++ Recursos/RecursosJS.js
@@ -2,26 +2,39 @@ google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(media_consultas);
 
 function media_consultas() {
-      var data = new google.visualization.DataTable();
-      data.addColumn('number', 'X');
-      data.addColumn('number', 'Média');
+  var data = google.visualization.arrayToDataTable([
+    ['Ano', 'Média de Consultas'],
+    ['2010',  15,],
+    ['2011', 10,],
+    ['2012',  23,],
+    ['2013',  17,],
+    ['2014',  23,],
+    ['2015',  9,],
+    ['2016',  11,],
+    ['2017',  27,],
+    ['2018',  33,],
+    ['2019',  45,],
+    ['2020',  46,],
+    ['2021',  51,],
+    ['2022',  55,],
+    ['2023',  57,],
+    ['2024',  60,],
+    ['2025',  63,],
+    ['2026',  65,],
+    ['2027',  67,],
+    ['2028',  63,],
+    ['2029',  70],
+    ['2030',  72],
+    ['2031',  77]
 
-      data.addRows([
-        [2010, 15],   [2011, 10],  [2012, 23],  [2013, 17],  [2014, 18],  [2015, 9],
-        [2016, 11],  [2017, 27],  [2018, 33],  [2019, 40],  [2020, 32], [2021, 35],
-        [2022, 30], [2023, 40], [2024, 42], [2025, 47], [2026, 44], [2027, 48],
-        [2028, 52], [2029, 54], [2030, 42], [2031, 55], 
-      ]);
 
-      var options = {
-        hAxis: {
-          title: 'Anos'
-        },
-        vAxis: {
-          title: 'Consultas'
-        },
-      };
-
+  ]);
+  var options = {
+    title: 'Média de Consultas',
+    curveType: 'function',
+    legend: { position: 'bottom' }
+  };
+  
       var chart = new google.visualization.LineChart(document.getElementById('chart_media_consultas'));
       chart.draw(data, options);
     }
